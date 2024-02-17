a1b1bf9c52f6778ef0e046062b0aecfc2fca0e51
wenderclaytonfilho <rednew2727@gmail.com>
2024-02-17 01:18:38 -0300

    Atention needed - firebase bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-import firebase from 'firebase/app'
+import firebase from 'firebase/compat/app'
 
-import 'firebase/firestore'
-import 'firebase/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
 
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
@@ -24,7 +24,7 @@ const firestore = firebase.firestore()
 
 function App() {
 
-  
+  const[user] = useAuthState(auth);
 
   return (
     <div className="App">
@@ -32,10 +32,53 @@ function App() {
 
       </header>
       <section>
-        {user ? <ChatRoom/> : <SignIn/>}
+        {user ? <ChatRoom /> : <SignIn />}
       </section>
     </div>
   );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () =>{
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut(){
+  return auth.currentUser && (
+    <button onClick={()=> auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom(){
+  const messagesRef = firestore.collection('messages')
+  const query = messagesRef.orderBy('createdAt').limit(25)
+
+  const [messages] = useCollectionData(query,{idField: 'id'})
+
+  return(
+    <>
+    <div>
+      {messages && messages.map(msg=> <ChatMessage key={msg.id} message={msg}/>)}
+    </div>
+
+    <div>
+
+    </div>
+    </>
+  )
+}
+
+function ChatMessage(props){
+
+  const {text, uid } = props.message
+  return <p>{text}</p>
 }
 
 export default App;
